3ce6483259b089809a3c43ddb64f326238bc232e
isanctuarry <khrnsa143@gmail.com>
2025-08-29 11:10:31 +0700

    ubah Dockerfile save index2

---
+++ home.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html lang="en"> 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+</head>
+<body>
+  <h1>bye :3</h1>
+  <p>Click to the link below to the main page c:</p>
+  <a href="index2.php">Main Page</a>
+</body>
+  
+</html>
--- index2.php
+++ index2.php
@@ -1,3 +1,9 @@
-<?php 
-"Hello, tugas pertama praktikum OOP"
-?> 
+<html>
+  <head>
+    <title>Tugas PPBO</title>
+  </head>
+  <body>
+    <h1>hi, selamat datang di tugas praktikum OOP 1</h1>
+    <?php echo '<p>coba pencet</p>'; ?> 
+    <a href="home.php">Home</a>
+</html>
