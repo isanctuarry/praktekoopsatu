e9f537e6b7e7ae8370238631f8f3c82c32a8ebc2
isanctuarry <khrnsa143@gmail.com>
2025-09-23 19:03:08 +0700

    edit

--- praktikum6.php
+++ praktikum6.php
@@ -5,23 +5,23 @@ class Bunga {
     // Constructor → otomatis dijalankan saat objek dibuat
     public function __construct($namaBunga) {
         $this->nama = $namaBunga;
-        echo "Bunga {$this->nama} telah mekar.<br>";
+        echo "Bunga {$this->nama} telah mekar.<br> <br>";
     }
 
     // Destructor → otomatis dijalankan saat objek dihapus / program selesai
     public function __destruct() {
-        echo "Bunga {$this->nama} dihancurkan.<br>";
+        echo "Bunga {$this->nama} dihancurkan.<br> <br>";
     }
 
     public function tampilkan() {
-        echo "Ini adalah bunga: {$this->nama}.<br>";
+        echo "Ini adalah bunga: {$this->nama}.<br> <br>";
     }
 }
 
 // Membuat objek
 $bunga1 = new Bunga("Bluebell");
-$bunga1->tampilkan();
+$bunga1->tampilkan() . "<br>";
 
 $bunga2 = new Bunga("Lavender");
-$bunga2->tampilkan();
+$bunga2->tampilkan() . "<br>";
 ?>
