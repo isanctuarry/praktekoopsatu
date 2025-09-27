38d4571d8ae82758c62836f7718bd82ace7ccaed
isanctuarry <khrnsa143@gmail.com>
2025-09-27 11:01:05 +0700

    newnew

--- tugasoopteori3.php
+++ tugasoopteori3.php
@@ -1,16 +1,26 @@
 <?php
 
-abstract class  Kendaraan {
-abstract public function bergerak();
-}
+            abstract class  Kendaraan {
+            abstract public function jalan();
+            }
 
-class Mobil extends Kendaraan {
-    public function bergerak() {
-        echo "Mobil berjalan dengan 4 roda <br>";
-    }}
+            class Mobil extends Kendaraan {
+                public function jalan() {
+                    echo "Mobil Porsche Taycan berjalan dengan 4 roda <br>";
+            }
+            }
 
-class Motor extends Kendaraan {
-    public function bergerak() {
-        echo "Motor bergerak dengan 2 roda <br>";
-}
-}
+            class Motor extends Kendaraan {
+                public function jalan() {
+                    echo "Motor Yamaha YZF-R1 berjalan dengan 2 roda <br>";
+            }
+            }
+
+            $motor1 = new Motor();
+            $motor1->jalan();
+            $mobil1 = new Mobil();
+            $mobil1->jalan();
+
+    
+
+
